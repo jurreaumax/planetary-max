7f1d26ebbd8fe03c5fa92ddf2c41d76a4ea512d1
jurreaumax <jurreaumax@proton.me>
2026-09-20 10:06:50 -0500

    Delete portal-os-gui directory

--- portal-os-gui/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Shell from "./layout/Shell";
-import "./layout/desktop.css";
-import "./layout/dock.css";
-
-export default function App() {
-  return <Shell />;
-}
--- portal-os-gui/src/api/identity.js
+++
@@ -1,5 +0,0 @@
-import { API } from "./client";
-
-export async function fetchIdentity() {
-  return API.get("/api/umbrella/identity");
-}
